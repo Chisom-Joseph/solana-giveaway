c0a5b8e36467d152b9c33f1d6a1a6292d447311f
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-12-05 22:22:30 +0100

    Changed site titles and corrected some spellin erros in the home page

--- routes/pages.js
+++ routes/pages.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
   res.render("home", {
     walletAddress: process.env.WALLET_ADDRESS,
+    title: "Solana Giveaway",
   });
 });
 
